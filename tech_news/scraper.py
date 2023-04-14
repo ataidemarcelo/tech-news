b677f1795a6e66d5b45cd9ad0ac88ec5587f140f
Marcelo Ataíde <msa.trybe@gmail.com>
2023-04-14 13:16:54 -0300

    Requisito 1 - Cria a função `fetch`

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,26 @@
 # Requisito 1
+import requests
+import time
+
+BASE_URL = 'https://blog.betrybe.com/'
+
+HEADERS = {"user-agent": "Fake user-agent"}
+
+
+# Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            BASE_URL,
+            headers=HEADERS,
+            timeout=3
+        )
+        time.sleep(1)
+
+        if response.status_code == 200:
+            return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
@@ -21,3 +41,8 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == '__main__':
+#     res = fetch(BASE_URL)
+#     print(res)
